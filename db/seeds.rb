de4ac746150181fb2c83f5a26f26cc9c5af90394
HAYASHInnn <fffm572752@gmail.com>
2025-06-26 20:25:34 +0900

    ダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+john = User.create!(email: 'john@example.com', password: 'password')
+emily = User.create!(email: 'emily@example.com', password: 'password')
+
+5.times do
+    john.boards.create!(
+        title: Faker::Lorem.sentence(word_count: 5),
+        description: Faker::Lorem.sentence(word_count: 15)
+        )
+end
+
+5.times do
+    emily.boards.create!(
+        title: Faker::Lorem.sentence(word_count: 5),
+        description: Faker::Lorem.sentence(word_count: 15)
+        )
+end
