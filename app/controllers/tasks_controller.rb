2ccac1864a763ca3cd73fb75942b591a86e103ba
HAYASHInnn <fffm572752@gmail.com>
2025-07-06 16:21:36 +0900

    Revert "createアクションの追加"
    This reverts commit 7fb870c8d0389b4d0cb5a54722d790647fe99fae.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,20 +6,4 @@ class TasksController < ApplicationController
         board = Board.find(params[:board_id])
         @task = board.tasks.build(user: current_user)
     end
-
-    def create
-        board = Board.find(params[:board_id])
-        @task = board.tasks.build(task_params.merge(user: current_user))
-        if @task.save
-            redirect_to board_path(board), notice: "タスクを追加しました"
-        else
-            flash.now[:error] = '更新できませんでした'
-            render :new, status: :unprocessable_entity
-        end
-    end
-
-    private
-    def task_params
-        params.require(:task).permit(:title, :content, :deadline)
-    end
 end
