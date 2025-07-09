b0a9138bafcafc186a93462587487a787c4d75a5
HAYASHInnn <fffm572752@gmail.com>
2025-07-09 15:26:36 +0900

    createを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,20 @@ class CommentsController < ApplicationController
         @task = Task.find(params[:task_id])
         @comment = @task.comments.build(user: current_user)
     end
+
+    def create
+        @task = Task.find(params[:task_id])
+        @comment = @task.comments.build(comment_params.merge(user: current_user))
+        if @comment.save
+            redirect_to board_task_path(@task.board, @task), notice: 'コメントを追加しました'
+        else
+            flash.now[:error] = 'コメントの追加に失敗しました'
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
 end
